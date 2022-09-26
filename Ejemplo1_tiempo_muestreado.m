ff96f3a0fa8ed61986fe63eabb0733a641617c3f
brian <brian@DESKTOP-DD45S43>
2022-09-26 17:09:28 -0300

    V1.0

--- Ejemplo1_tiempo_muestreado.m
+++ Ejemplo1_tiempo_muestreado.m
@@ -1,15 +1,12 @@
 Nseq=47; %Longitud de la secuencia
 root=5; %semilla de la secuencia
 ZCseq=zadoffChuSeq(root,Nseq)';
-figure(4);
-plot(real(ZCseq),imag(ZCseq),'x');
-grid on;
 nRound=6;
 
-%A=randomMatrix(1,21);
-%B=randomMatrix(1,30);
+A=randomMatrix(1,21);
+B=randomMatrix(1,30);
 
-totalSeq=ZCseq; %Con ruido
+totalSeq=[A ZCseq B]; %Con ruido
 %%
 fc=110e3; %frecuencia de la señal portadora
 tc=1/fc; %periodo de la señal portadora
@@ -17,11 +14,9 @@ N=10; %Muestras por ciclo
 nTemp= 100; %Numero de puntos por ciclo
 t1=0:tc/nTemp:tc-tc/nTemp; %paso temporal en un ciclo de la portadora
 t=0:tc/nTemp:length(totalSeq)*tc-tc/nTemp; %vector temporal de secuencia completa
-sI=sin(2*pi*fc*t1); %onda cuadrada en fase
-sQ=sin(2*pi*fc*t1+pi/2); %onda cuadrada en cuadratura
+sI=square(2*pi*fc*t1); %onda cuadrada en fase
+sQ=square(2*pi*fc*t1+pi/2); %onda cuadrada en cuadratura
 
-% figure;
-% plot(t1,sQ);
 %%
 xI=reshape(kron(imag(totalSeq),sI)',1,[]);
 xQ=reshape(kron(real(totalSeq),sQ)',1,[]);
@@ -31,14 +26,14 @@ modSeq=xI-xQ;
 
 [c1,lag]=xcorr(modSeq,modZCseq);
 
-figure(1);
-plot(t,modSeq);
-
-figure(2);
-plot(lag,c1/max(abs(c1)));
+figure;
+plot(t,modSeq);title("Señal temporal modulada");
 
 figure;
-fourier_transform(modSeq,fc*nTemp,'frec')
+plot(lag,c1/max(abs(c1)));title("Correlación ideal (sin muestrear)");
+
+figure;
+fourier_transform(modSeq,fc*nTemp,'frec');title("TF de la secuencia modulada")
 
 ts=0:tc/N:length(totalSeq)*tc-tc/nTemp;
 sampledZCseq=zeros(1,length(modZCseq)*N/nTemp);
@@ -46,8 +41,8 @@ for i=0:length(sampledZCseq)-1
     sampledZCseq(1+i)=modZCseq(1+i*nTemp/N);
 end
 figure;
-subplot(1,2,1);plot(sampledZCseq)
-subplot(1,2,2);plot(modZCseq)
+subplot(2,1,1);plot(modZCseq);title("Secuencia ZC modulada");
+subplot(2,1,2);plot(sampledZCseq);title("Secuencia ZC modulada y muestreada");
 cola=zeros(1,length(sampledZCseq));
 c=zeros(1,length(t)*N/nTemp);
 
@@ -56,8 +51,8 @@ for i=0:length(t)*N/nTemp-1
     c(1+i)=xcorr(cola,flip(sampledZCseq),0);
 end
 
-figure(3);
-plot(ts,c);
+figure;
+plot(ts,c);title("Correlación de las muestras");
 
 function R=randomMatrix(m,n)
     R=zeros(m,n);
