4695bc0d4b48b085f4827e503c6eb384986f7e04
briangbx <brianfarias9912@gmail.com>
2023-03-05 19:32:44 -0300

    Se solucionó un error en la simulación de la cuantización. Ahora el número de bits de la conversión define correctamente la resolución. Se agrega ruido blanco y se concluye el analisis para las dos posibles portadoras.

--- correlador_ZC.m
+++ correlador_ZC.m
@@ -1,16 +1,13 @@
 clear
 close all
 
-Nseq=313; %Longitud de la secuencia
-root=1; %semilla de la secuencia
+Nseq=149; %Longitud de la secuencia
+root=17; %semilla de la secuencia
 ZCseq=zadoffChuSeq(root,Nseq)';
-bitRound=4; %número de bits del conversor
-%nSim=1;
+bitRound=10; %número de bits del conversor
+nSim=1;      %número de ciclos por símbolo
 
-A=randomMatrix(1,21);
-B=randomMatrix(1,30);
-
-totalSeq=ZCseq; %Con ruido
+totalSeq=ZCseq;
 
 fc=110e3; %frecuencia de la señal portadora
 tc=1/fc; %periodo de la señal portadora
@@ -33,37 +30,35 @@ xQ2=reshape(kron(real(totalSeq),sQ2)',1,[]);
 modSeq1=xI1-xQ1; %Secuencia modulada con onda cuadrada
 modSeq2=xI2-xQ2; %Secuencia modulada con onda senoidal
 
+min1=min(modSeq1);min2=min(modSeq2);
+max1=max(modSeq1);max2=max(modSeq2);
+
 figure
-subplot(1,3,1);plot(modSeq1);axis([140 170 -inf inf]) 
-modSeq1=round((modSeq1-min(modSeq1))/(max(modSeq1)-min(modSeq1))*2^bitRound)-2^(bitRound-1);
-modSeq2=round((modSeq2-min(modSeq2))/(max(modSeq2)-min(modSeq2))*2^bitRound)-2^(bitRound-1);
+subplot(1,4,1);plot(modSeq1);axis([140 170 -inf inf]) 
+modSeq1=round((modSeq1-min1)/(max1-min1)*(2^bitRound-1))-(2^(bitRound-1)-1/2);  %Se simula el efecto de cuantizar la secuencia
+modSeq2=round((modSeq2-min2)/(max2-min2)*(2^bitRound-1))-(2^(bitRound-1)-1/2);
 hold on
-% subplot(1,3,2);
+subplot(1,4,2);
 plot(modSeq1);axis([140 170 -inf inf]) 
 
-%modSeq1=modSeq1*4/(2^bitRound)-2; %Redondeo de las secuencias
-%modSeq2=modSeq2*2/(2^bitRound)-1;
+modSeq1=modSeq1/(2^bitRound-1); %Se devuelve a la secuencia a valores aproximadamente de la misma magnitud original
+modSeq2=modSeq2/(2^bitRound-1);
 
-subplot(1,3,3);plot(modSeq1);axis([140 170 -inf inf]) 
+subplot(1,4,3);plot(modSeq1);axis([140 170 -inf inf])
+
+modSeq1=awgn(modSeq1,100,'measured');
+modSeq2=awgn(modSeq2,100,'measured');
+
+subplot(1,4,4);plot(modSeq1);axis([140 170 -inf inf])
 
 modZCseq1=reshape(kron(imag(ZCseq),sI1)',1,[])-reshape(kron(real(ZCseq),sQ1)',1,[]);
 modZCseq2=reshape(kron(imag(ZCseq),sI2)',1,[])-reshape(kron(real(ZCseq),sQ2)',1,[]);
 
-figure;
-subplot(1,2,1);plot(real(ZCseq));
-subplot(1,2,2);plot(modZCseq1);
-
-% modZCseq1=round(((modZCseq1+2)/4)*2^bitRound);
-% modZCseq2=round(((modZCseq2+1)/2)*2^bitRound);
-% 
-% modZCseq1=modZCseq1*4/(2^bitRound)-2;
-% modZCseq2=modZCseq2*2/(2^bitRound)-1;
-
 [c1,lag1]=xcorr(modSeq1,modZCseq1); %correlación de las señales
-[c2,lag2]=xcorr(modSeq1,modZCseq2);
+[c2,lag2]=xcorr(modSeq2,modZCseq2);
 
-c1=c1/(abs(max(c1)));
-c2=c2/(abs(max(c2)));
+%c1=c1/(abs(max(c1)));
+%c2=c2/(abs(max(c2)));
 
 figure;
 subplot(2,1,1);plot(lag1,c1);title("Correlacion de la secuencia modulada con onda cuadrada");
@@ -77,12 +72,3 @@ figure;
 fourier_transform(modSeq1,fs,'frec');title("TF de la secuencia modulada con onda cuadrada")
 figure;
 fourier_transform(modSeq2,fs,'frec');title("TF de la secuencia modulada con onda senoidal")
-
-function R=randomMatrix(m,n)
-    R=zeros(m,n);
-    for i=1:m
-        for j=1:n
-            R(i,j)=exp(1i*2*pi*rand);
-        end
-    end
-end
